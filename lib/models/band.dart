f332f44182d635fe5cefc676d3f09fdec4d10b3a
juanadls <eleo.dev09@gmail.com>
2024-12-19 20:56:52 -0300

    band model created

---
+++ lib/models/band.dart
@@ -0,0 +1,19 @@
+class Band {
+  Band({
+    required this.id,
+    required this.name,
+    required this.votes,
+  });
+
+  String id;
+  String name;
+  int votes;
+
+  factory Band.fromJson(Map json) {
+    return Band(
+      id: json["id"],
+      name: json["name"],
+      votes: json["votes"],
+    );
+  }
+}
